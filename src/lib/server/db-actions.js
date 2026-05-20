8bce819ddada2c4723714f11f9b1e633b37fa28d
panara.harsh <panara.harsh@silvertouch.com>
2026-05-20 18:39:31 +0530

    convertToSectionList

--- src/lib/server/db-actions.js
+++ src/lib/server/db-actions.js
@@ -66,3 +66,21 @@ export const clearPurchasedItems = async () => {
 };
 
 
+export const convertToSectionList = (array, groupByKey) => {
+  return Object.values(
+    array.reduce((acc, item) => {
+      const key = item[groupByKey];
+
+      if (!acc[key]) {
+        acc[key] = {
+          title: key,
+          data: [],
+        };
+      }
+
+      acc[key].data.push(item);
+
+      return acc;
+    }, {}),
+  );
+};
